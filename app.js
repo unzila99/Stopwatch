c134cbada434831277ccf1b2f8645544c40d7a66
Unzila <unzilasidd901@gmail.com>
2020-07-10 22:40:00 +0500

    Stopwatch

---
+++ app.js
@@ -0,0 +1,45 @@
+var min = 00;
+var sec = 00;
+var msec = 00;
+
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
+var interval;
+
+
+function timer(){
+    msec++
+    msecHeading.innerHTML = msec;
+    if (msec >= 100){
+        sec++
+        secHeading.innerHTML = sec;
+        msec = 0;
+    }
+    else if (sec >= 60){
+        min++
+        sec = 0;
+        minHeading.innerHTML = min;
+    }
+}
+interval = setInterval(timer,10)
+
+
+function start(){
+    clearInterval(interval)
+    interval = setInterval(timer,10)
+}
+
+function stop(){
+    clearInterval(interval)
+}
+
+function reset(){
+    min = 00;
+    sec = 00;
+    msec = 00;
+    minHeading.innerHTML = min
+    secHeading.innerHTML = sec
+    msecHeading.innerHTML = msec
+    stop();
+}
